b5dacd0bde726b23cc4352bf013e6de37fe48b58
Lukas Manriquez <lukas.manriquez@my.tccd.edu>
2024-09-23 21:13:58 -0500

    quiz time fix later

--- classwork/chapter4/movie liberty/movie liberty.cpp
+++ classwork/chapter4/movie liberty/movie liberty.cpp
@@ -1,3 +1,4 @@
+// file header must be on top it must be on line 1
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -20,6 +21,15 @@ struct Movie
     int ReleaseYear;
 
     bool IsClassic;
+};
+
+enum MenuCommand
+{
+    MC_Addmovie,
+    MC_editmovie,
+    MC_Deletemovie,
+    MC_Viewmovie,
+
 };
 
 int main()
@@ -56,7 +66,48 @@ int main()
   // cout << left << " >= " << right << " = " << (left >= right) << endl;
   // cout << left << " != " << right << " = " << (left != right) << endl;
   // cout << endl;
-  
+
+    //show menu
+    cout << "Movie Library" << endl;
+    cout << "----------------" << endl;
+    cout << "A)dd movive" << endl;
+    cout << "E)dit Movie" << endl;
+    cout << "D)elete Movie" << endl;
+    cout << "V)iew Movie" << endl;
+
+    //get input
+    char input;
+    cin >> input;
+
+    MenuCommand menuCommand
+
+    switch (input)
+    {
+        case 'A':
+        case 'a': menuCommand = 1; break;
+
+        case 'E':
+        case 'e': menuCommand = 2; break;
+
+        case 'D':
+        case 'd': menuCommand = 3; break;
+
+        case 'V':
+        case 'v': menuCommand = 4; break;
+
+        default: cout << "Bad input" << endl; break;
+    }
+
+    /// handle menu command
+   
+
+    switch (menuCommand)
+    {
+        case  MenuCommand::MC_Addmovie:
+        case  MenuCommand::MC_editmovie:
+        case  MenuCommand::MC_Deletemovie:
+        case  MenuCommand::MC_Viewmovie: cout << "not implemeted" << end; break;
+    }
 
   // decision making - IF statment 
   //if-stmt ::= 
@@ -122,7 +173,7 @@ int main()
 
         cout << "Enter Release yesr (1900+): ";
         cin >> movie.ReleaseYear;
-    }
+    };
 
     //get optional description
     cout << "Enter optional description: ";
@@ -143,27 +194,79 @@ int main()
     else if (isClassic == 'n')
      movie.IsClassic = false;
     else*/
-
-    if (isClassic == 'Y' || isClassic == 'y')
-    movie.IsClassic = true;
+    // not bad but not short
+   /* if (isClassic == 'Y' || isClassic == 'y')
+        movie.IsClassic = true;
     else if (isClassic == 'N' || isClassic == 'n')
-    movie.IsClassic = false;
+        movie.IsClassic = false;
     else
     {
-        cout << "ERROR: You must enter Y or N "; 
+        cout << "ERROR: You must enter Y or N ";
 
         cout << "Is this a classic (Y/N)";
         cin >> isClassic;
-    }
+    };*/
+    
+   //Switch (select) statement - replacement for if-elseif where same expression compared to multiple values
+    // 1. must compare a single expression to one or more constant vlues with qeuility
+    // 2. switch express must be an intergral type
+    // 3. each case label must be a compile-time constant expression 
+    // 4. each case label must be unique
+    // default must be last and will accivate if nothing else went in
+    // put    break; it will break if u dont
+    switch (isClassic)
+            {
+        case 'Y':movie.IsClassic = true; break;
+        case 'y':movie.IsClassic = true; break;
+
+        case 'N':movie.IsClassic = true; break;
+        case 'n':movie.IsClassic = true; break;
+
+        default:;
+        {
+            cout << "ERROR: You must enter Y or N ";
+
+            cout << "Is this a classic (Y/N)";
+            cin >> isClassic;
+        }
+};
+    /*
+
+    else
+    {
+        cout << "ERROR: You must enter Y or N ";
+
+        cout << "Is this a classic (Y/N)";
+        cin >> isClassic;
+    };*/
 
     // display movie detals
     cout << "---------------" << endl;
     cout << movie.Title << " (" << movie.ReleaseYear << ")" << endl;
     cout << "Run Length (mins) " << movie.RunLength << endl;
+
+    //long form -1
+    // if (movie.IsClassic)
+    //    cout << "Is Classic? Yes" << endl;
+    //  else
+    //    cout << "Is Classic? No" << endl;
+    /*better but longer -2
+    string classicIndicator;
     if (movie.IsClassic)
-        cout << "Is Classic? yes " << endl;
+        classicIndicator = "Yes";
     else
-        cout << "is Classic? no" << endl;
+        classicIndicator = "No";
+    cout << "is Classic? " << classicIndicator << endl;
+    */
+    
+    //shorter form using conditional operator => eb ? et : ef
+   // string classicIndicator = movie.IsClassic ? "Yes" : "no";
+   // cout << "Is Classic? " << classicIndicator << endl;
+
+    //shortest  form using conditional operator => Eb ? Et : Ef
+    // Et & Ef = must be the exat same type, type coercion is not allowed
+    cout << "Is Classic? " << (movie.IsClassic ? "yes" : "No") << endl;
+
     if (movie.Description != "")
         cout << movie.Description << endl;
     cout << "---------------" << endl;
