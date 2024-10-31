d4f8fd6b5ed8f59b51c87f4bae31fc07a05eb649
Lukas Manriquez <lukas.manriquez@my.tccd.edu>
2024-10-30 21:24:31 -0500

    something  -+R+

--- classwork/chatper7/chatper7/chatper7.cpp
+++ classwork/chatper7/chatper7/chatper7.cpp
@@ -52,6 +52,66 @@ void TestPassByReference()
     Display(rc);
 }
 
+//functio paramitors 'T id[]'
+//c++ arrays are always open, no size
+//size is always needed as param when using arrays
+bool AreArrayEqual( int left[],int leftSize, int right[], int rightSize )
+{
+    if (leftSize != rightSize)
+        return false;
+
+    for (int index = 0; index < leftSize; ++index)
+        if (left[index] != right[index])
+            return false;
+
+    return true;
+}
+int copyArray(int left[], int leftSize, int right[], int rightSize)
+{
+    int size = (leftSize < rightSize) ? leftSize : rightSize;
+    for (int index = 0; index < size; ++index)
+    left[index] = right[index];
+
+    return size;
+}
+
+// array cannot be retuen type from function
+void InitializeArray(int values[], int size, int initialValue)
+{
+    for (int index = 0; index < size; ++index)
+        values[index] = initialValue;
+}
+void useArrayDemo()
+{
+    int array1[20] = {0};
+
+   // for (int index = 0; index < 20; ++index)
+     //   array1[index] = index + 1;
+   /* for (int& value : array1)
+        value = 1;*/
+    InitializeArray(array1, 20, 2);
+    //int index2 = 0;
+    //while (index2 < 20)
+    //{
+    //    array1[index2] = ++index2;
+    //};
+
+    // for loop for iterrating arrays 
+   /* for (int index = 0; index < 20; ++index)
+     std::cout << array1[index] << std::endl;  */
+    for (int value : array1)
+        std::cout << value << std::endl;
+
+    int array2[20] = {0};
+    int array3[20] = {0};
+    //compare arrays
+    std::cout << (array2 == array3) << " " << AreArrayEqual(array2, 20, array3, 20) << std::endl;
+
+    //assighnment
+    /*array2 = array3;*/
+    copyArray(array3, 20, array2, 20);
+}
+
 void InitArrayDemo()
 {
     const int MaxRates = 100;
@@ -59,6 +119,22 @@ void InitArrayDemo()
 
         //for (int index = 0; index < MaxRates; ++index)
         //    payRates[index] = 0
+        // Array min = 0 & 
+       // array max = size - 1
+
+        //init days of month
+        
+        std::string monthNames[12] = {"Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",};
+        
+
+        int daysInMonth[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            //daysInMonth[0] = daysInMonth[2] = daysInMonth[4] = daysInMonth[6] = daysInMonth[7] = daysInMonth[9] = daysInMonth[11] = 31;
+            //daysInMonth[1] = 28;
+            //daysInMonth[3] = daysInMonth[5] = daysInMonth[8] = daysInMonth[10] = 30;
+        for (int index = 0; index < 12; ++index)
+        std::cout << monthNames[index] << " has " << daysInMonth[index] << " days" << std::endl;
+
+
 }
 void GradeArrayDemo()
 {
@@ -105,7 +181,7 @@ void GradeArrayDemo()
 
 int main()
 {
-    InitArrayDemo();
+    useArrayDemo();
 }
  
 void NameArrayDemo() 
@@ -144,3 +220,4 @@ void NameArrayDemo()
     }
      
 }
+
